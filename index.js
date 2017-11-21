3e316e6dec2720ac3d5283488c9dea0359e9ed59
Brad Bowie <brad@itenneti.com>
2017-11-21 14:54:07 -0500

    feat: support for email and password validation

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const isEmail = require('email-addresses').parseOneAddress
+
 // must start with letter
 // must be 3-64 characters
 // can only contain lowercase letters, numbers, underscore, or hyphen
@@ -9,6 +11,20 @@ function isValidSlug (str) {
   return !!str && SLUG_REGEX.test(str)
 }
 
+// can contain any characters
+// must be 6-64 characters
+const PASSWORD_REGEX = /^.{6,64}$/
+function isValidPassword (str) {
+  return !!str && PASSWORD_REGEX.test(str)
+}
+
+// just re-use this other person's work
+function isValidEmail (str) {
+  return !!str && isEmail(str)
+}
+
 module.exports = {
-  isValidSlug
+  isValidSlug,
+  isValidPassword,
+  isValidEmail
 }
